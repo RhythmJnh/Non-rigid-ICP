a6aeaa452022d55baf6c08db23ae4fbc8d8cd112
Nianhong Jiao <1327362838@qq.com>
2019-03-30 12:53:20 +0800

    Update faceDemo.m

--- demos/faceDemo.m
+++ demos/faceDemo.m
@@ -15,8 +15,19 @@ Options.useNormals = 1;
 % Specify that the source deformations should be plotted.
 Options.plot = 1;
 
+% Specify that the landmarks term are available and can be used.
+Options.landmarks = 0;
+
+% Selected landmarks
+ls_source = [];
+ls_target = [];
+
 % Perform non-rigid ICP
-[pointsTransformed, X] = nricp(Source, Target, Options);
+[pointsTransformed, X] = nricp_landmarks(Source, Target, Options, ls_source, ls_target);
 faces = double(Source.faces);
 save('../output/point_cloud/vertices_demo.txt', 'pointsTransformed', '-ascii');
 save('../output/point_cloud/faces_demo.txt', 'faces', '-ascii');
+
+% Save obj file
+filename = '../demo.obj';
+saveObj(filename, pointsTransformed, faces);
